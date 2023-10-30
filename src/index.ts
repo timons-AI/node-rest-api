745f2b7de43b1536e8119ce450e0d8ef18f71134
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-10-30 13:59:27 +0300

    Mongo DB setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -22,3 +23,10 @@ const server = http.createServer(app);
 server.listen(8080, () => {
   console.log("Server running on http://localhost:8000");
 });
+
+const MONGO_URL =
+  "mongodb+srv://timothy:@3I4RBU85t12OKhGtcluster0.thgacpv.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
