4326c105d84f8ed4ba470a9666dec068536f54de
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-10-30 14:54:55 +0300

    authentication logging and routing

---
+++ src/router/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const router = express.Router();
+import authentication from "./authentication";
+export default (): express.Router => {
+  authentication(router);
+  return router;
+};
